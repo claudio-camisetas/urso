1f85c35cb3cc92a15b98e389cc0d21e47b643c7f
D-Freitas <davi.israel01@gmail.com>
2024-01-04 23:33:38 -0300

    feat: add shadow

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -9,15 +9,30 @@ import logo from '../images/logo.png'
 const inactivedEye = 'https://centralderequisicoes.einstein.br/static/media/icon_olho_fechado.4b41a025.svg'
 const activedEye = 'https://centralderequisicoes.einstein.br/static/media/icon_olho_aberto.9a36df1e.svg'
 
+function isValidCPF(cpf: string) {
+  cpf = cpf.replace(/\D/g, '')
+  if (cpf.length !== 11) return false
+  const invalidCPFs = Array.from({ length: 10 }, (_, i) => String(i).repeat(11));
+  if (invalidCPFs.includes(cpf)) return false
+  let total = cpf.split('').slice(0, 9).reduce((acc: number, digit: string, index: number) => acc + parseInt(digit) * (10 - index), 0) % 11;
+  const digit1 = total < 2 ? 0 : 11 - total;
+  total = cpf.split('').slice(0, 10).reduce((acc: number, digit: string, index: number) => acc + parseInt(digit) * (11 - index), 0) % 11;
+  const digit2 = total < 2 ? 0 : 11 - total;
+  return cpf.endsWith(String(digit1) + String(digit2));
+}
+
 export default function SignUp() {
   const [cpf, setCpf] = useState('')
   const [password, setPassword] = useState('')
   const [eye, setEye] = useState(inactivedEye)
+  const [isCpfValid, setIsCpfValid] = useState(true)
 
   const formatCpf = (event: ChangeEvent<HTMLInputElement>) => {
     const formatedCpf = event.target.value
       .replace(/\D/g, '')
       .replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+    setIsCpfValid(true)
+    if (formatedCpf.length === 14) setIsCpfValid(isValidCPF(formatedCpf))
     setCpf(formatedCpf)
   }
 
@@ -27,11 +42,8 @@ export default function SignUp() {
   }
 
   const changeEyeImage = () => {
-    if (eye === inactivedEye) {
-      setEye(activedEye)
-    } else {
-      setEye(inactivedEye)
-    }
+    if (eye === inactivedEye) setEye(activedEye)
+    else setEye(inactivedEye)
   }
 
   return (
@@ -53,7 +65,8 @@ export default function SignUp() {
             placeholder='DIGITE SEU CPF'
             value={cpf}
             onChange={formatCpf}
-            className={S.input}
+            className={isCpfValid ? S.input : S.input_invalid}
+            required
           />
           <input
             type={eye === inactivedEye ? 'password' : 'text'}
@@ -62,6 +75,7 @@ export default function SignUp() {
             value={password}
             onChange={formatedPassword}
             className={S.input}
+            required
           />
           <EyeButton
             imageUrl={eye}
@@ -84,21 +98,6 @@ export default function SignUp() {
   )
 }
 
-const SignUpContainer = styled.div`
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  background: #005ca9;
-  width: 440px;
-  height: 600px;
-  padding: 20px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  @media screen and (max-width: 450px) {
-    width: 90%;
-    overflow: scroll;
-  }
-`
-
 const EyeButton = styled.div<{ imageUrl: string }>`
   position: absolute;
   top: 94px;
@@ -110,37 +109,4 @@ const EyeButton = styled.div<{ imageUrl: string }>`
   &:hover {
     cursor: pointer;
   }
-`
-
-const Button = styled.button`
-  background: #fcb502;
-  height: 50px;
-  outline: 0;
-  border: 0;
-  color: #fff;
-  border-radius: 3px;
-  width: 100%;
-  margin: 20px 0;
-  font-weight: bold;
-  &:hover {
-    background: #fdbd1c;
-    cursor: pointer;
-  }
-  &:active {
-    background: #e3a302;
-  }
-`
-
-const Help = styled.div`
-  display: flex;
-  flex-direction: column;
-  color: #fff;
-  text-align: center;
-  font-size: 14px;;
-  margin: 10px 0;
-`
-
-const Version = styled.div`
-  color: #bcbfc0;
-  font-size: 16px;
 `
